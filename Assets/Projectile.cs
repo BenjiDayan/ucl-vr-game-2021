99604381139a5f5300315f97e1a7824652c1a0fa
diasporite <72630458+diasporite@users.noreply.github.com>
2021-01-26 14:06:43 +0000

    Add files via upload

---
+++ Assets/Projectile.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(CapsuleCollider))]
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] protected bool affectedByGravity = false;
+
+    [SerializeField] protected float speed = 50;
+
+    [SerializeField] protected float lifetime = 1;
+
+    [SerializeField] protected Vector3 trajectory;
+
+    protected float lifetimeCount = 0;
+
+    Rigidbody rb;
+
+    public Vector3 _trajectory
+    {
+        get => trajectory;
+        set
+        {
+            trajectory.x = value.x;
+            trajectory.y = value.y;
+            trajectory.z = value.z;
+        }
+    }
+
+    public Rigidbody _rb => rb;
+
+    protected virtual void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        rb.useGravity = affectedByGravity;
+    }
+
+    protected virtual void Update()
+    {
+        lifetimeCount += Time.deltaTime;
+
+        if (lifetimeCount >= lifetime) Destroy(gameObject);
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + trajectory * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        int damage = 0;
+        var damageable = collision.gameObject.GetComponent<IDamageable>();
+
+        if (damageable != null)
+        {
+            damageable.OnDamage(damage);
+
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.position, trajectory);
+    }
+}
