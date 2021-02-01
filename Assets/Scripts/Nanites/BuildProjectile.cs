ec3a84e3063a77e812532c0c3aa59bbc9f9a3a52
AlecSoren <63143265+AlecSoren@users.noreply.github.com>
2021-02-01 14:40:55 +0000

    Building generation & creation systems
    Added system which procedurally generates buildings when game starts.
    
    Added system for player to collect debris and use it to create new buildings.

---
+++ Assets/Scripts/Nanites/BuildProjectile.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildProjectile : MonoBehaviour
+{
+
+    public GameObject buildingParent;
+    public GameObject buildingSegment;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        string colliderName = collision.gameObject.name;
+        if (colliderName != "Gun" && colliderName != "Player")
+        {
+            Build();
+            GameObject.Find("Gun").SendMessage("AddSegments", -49);
+            Destroy(gameObject);
+        }
+    }
+
+    void Build()
+    {
+        Vector3 myPosition = transform.position;
+        float originX = myPosition.x;
+        float originY = myPosition.z;
+
+        GameObject parentClone = Instantiate(buildingParent, new Vector3 (originX, 9, originY), Quaternion.identity);
+        parentClone.GetComponent<BoxCollider>().size = new Vector3 (9, 18, 9);
+
+        for (int x = -1; x < 2; x++)
+        {
+            for (int y = -1; y < 2; y++)
+            {
+                for (int z = 0; z < 6; z++)
+                {
+                    if
+                    (x != 0 || y != 0 || z == 5)
+                    {
+                        Instantiate
+                        (
+                            buildingSegment,
+                            new Vector3(
+                                originX + (x * 3),
+                                1.5f + (z * 3),
+                                originY + (y * 3)
+                                ),
+                            Quaternion.identity,
+                            parentClone.transform
+                        );
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Nanites/NaniteProjectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaniteProjectile : MonoBehaviour
+{
+
+    public GameObject naniteCloud;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        string colliderName = collision.gameObject.name;
+        if (colliderName != "Gun" && colliderName != "Player" && GameObject.Find("Nanite Cloud(Clone)") == null)
+        {
+            Instantiate(naniteCloud, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
