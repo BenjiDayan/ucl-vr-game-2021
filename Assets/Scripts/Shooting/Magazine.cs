b7b8c0c13ca61b85bedcba3f34a5abedd9d49efd
diasporite <72630458+diasporite@users.noreply.github.com>
2021-02-02 00:02:46 +0000

    Add files via upload

---
+++ Assets/Scripts/Shooting/Magazine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Magazine
+{
+    [SerializeField] int size;
+    [SerializeField] int ammo;
+
+    public int _size => size;
+    public int _ammo => ammo;
+
+    public Magazine(int size)
+    {
+        this.size = size;
+        ammo = size;
+    }
+
+    public void ChangeAmmo(int amount)
+    {
+        ammo += amount;
+        if (ammo > size) ammo = size;
+        if (ammo < 0) ammo = 0;
+    }
+}
