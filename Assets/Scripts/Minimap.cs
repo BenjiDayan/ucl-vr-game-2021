24d162ca1684687f5d757b4b5d22d518561a1ddd
athinaxen <athinaki2010@gmail.com>
2021-03-24 10:45:13 +0000

    Minimap

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    public Transform player;
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+        // camera rotates with player too
+        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+    }
+}
